44023010865dd819557bb74a018e23abae89def0
Nathan Fisher <nooglefish@gmail.com>
2017-01-30 22:05:19 +0000

    Update production config import

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 import devConfig from './config.dev'
 import testConfig from './config.test'
+import prodConfig from './config.prod'
 
 const config = function() {
     switch(process.env.NODE_ENV){
@@ -7,7 +8,7 @@ const config = function() {
             return devConfig
         case 'test':
             return testConfig
-        case 'prod':
+        case 'production':
             return prodConfig
         default:
             return devConfig
